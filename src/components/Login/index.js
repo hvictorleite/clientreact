58dc05d3f1fb3b4ea5d091d2cf000a51f5fda53b
hvictorleite <hvictorleite@gmail.com>
2023-01-03 11:33:50 -0300

    Craida página de login. Estilo global criado.

---
+++ src/components/Login/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './styles.css';
+import logoImage from '../../assets/login.png';
+
+function Login() {
+    return (
+        <div className="login-container">
+            <section className="form">
+                <img src={logoImage} alt="Login" id="img1" />
+
+                <form>
+                    <h1>Cadastro de Alunos</h1>
+                    <input placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <button class="button" type="submit">Login</button>
+                </form>
+            </section>
+        </div>
+    );
+}
+
+export default Login;
